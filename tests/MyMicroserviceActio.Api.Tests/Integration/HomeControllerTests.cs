4a9b7f8e2983e7a723cf382b73dd07371972a3a2
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-11-21 01:50:23 +0100

    Bump Microsoft.AspNetCore.TestHost version

--- tests/MyMicroserviceActio.Api.Tests/Integration/HomeControllerTests.cs
+++ tests/MyMicroserviceActio.Api.Tests/Integration/HomeControllerTests.cs
@@ -10,25 +10,25 @@ namespace MyMicroserviceActio.Api.Tests.Integration
 {
     public class HomeControllerTests
     {
-        //private readonly TestServer _server;
-        //private readonly HttpClient _client;
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
 
-        //public HomeControllerTests()
-        //{
-        //    _server = new TestServer(WebHost.CreateDefaultBuilder()
-        //        .UseStartup<Startup>());
-        //    _client = _server.CreateClient();
-        //}
+        public HomeControllerTests()
+        {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
 
-        //[Fact]
-        //public async Task home_controller_get_should_return_string_content()
-        //{
-        //    var response = await _client.GetAsync("/");
-        //    response.EnsureSuccessStatusCode();
+        [Fact]
+        public async Task home_controller_get_should_return_string_content()
+        {
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
 
-        //    var content = await response.Content.ReadAsStringAsync();
+            var content = await response.Content.ReadAsStringAsync();
 
-        //    content.Should().BeEquivalentTo("Hello from MyMicroserviceActio API!");
-        //}
+            content.Should().BeEquivalentTo("Hello from MyMicroserviceActio API!");
+        }
     }
 }
